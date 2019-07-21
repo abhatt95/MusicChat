d0279f94fe3518a10850234ac3e0a21f4d6a6d80
Abhishek Bhatt <abhipbhatt@yahoo.co.in>
2019-07-21 12:41:42 +0530

    Upload to heroku

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+  //console.log(__dirname + '/index.html');
+  //console.log(__dirname + '/piano.mp3');
+
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    socket.broadcast.emit('chat message', msg );
+    //io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
